ba617e4221e174de1b7c85bd9fd9bdefb97534bf
0kam <maisha.marley@gmail.com>
2021-02-15 23:41:24 +0900

    modified docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../src/alproj/'))
+
+sys.path.insert(0, os.path.abspath('../src/'))
 
 
 # -- Project information -----------------------------------------------------
