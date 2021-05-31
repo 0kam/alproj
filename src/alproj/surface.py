4b648abff73ea7cc74dfef74e62aead141295299
0kam <maisha.marley@gmail.com>
2021-05-31 19:49:09 +0900

    test

--- src/alproj/surface.py
+++ src/alproj/surface.py
@@ -89,7 +89,6 @@ def create_db(aerial, dsm, out_path, res=1.0, chunksize=10000):
     del(df)
     conn.close()
 
-
 def crop(conn, params, distance=3000, chunksize=100000):
     """
     Crops the given surface in fan shape.
@@ -131,7 +130,7 @@ def crop(conn, params, distance=3000, chunksize=100000):
     corners = np.vstack([x1,y1,np.ones(4)])
     corners = distort(corners, p["a1"], p["a2"], p["k1"], p["k2"], p["k3"], p["k4"], p["k5"], p["k6"], p["p1"], p["p2"], p["s1"], p["s2"], p["s3"], p["s4"])
     x = corners[0, :] * centre[0] ## Modified from coners[0, :] * centre[1]
-    fov = params["fov"] * params["w"] / (max(x) - min(x))
+    fov = params["fov"] /  params["w"] * (max(x) - min(x))
     pan = params["pan"] + math.atan2(( (max(x) + min(x)) / 2 ) / centre[0], 1) * 180 / math.pi
 
     params = {"x":str(params["x"]),"y":str(params["y"]),"pan":str(pan), "fov":str(fov)}
